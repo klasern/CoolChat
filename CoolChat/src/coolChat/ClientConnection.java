73df7a925c5189304f3e6fea434e906e30a4210f
klasern <klasern@karmosin-12.csc.kth.se>
2018-02-14 11:38:11 +0100

    Adds ChatListenr, and starts writing the body of ClientConnection and ServerConnection

--- CoolChat/src/coolChat/ClientConnection.java
+++ CoolChat/src/coolChat/ClientConnection.java
@@ -1,14 +1,52 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ClientConnection
+ * 
+ * V 1.0
+ *
+ * 2018-01-17
+ * 
+ * Copyright notice
  */
 package coolChat;
 
+import java.io.*;
+import java.net.*;
+
 /**
- *
- * @author klasern
+ * Used to establish a connection with a server.
  */
 public class ClientConnection extends Thread {
+
+    private Socket mySocket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    private Chat myChat;
+    private UserView myUserView;
+
+    /**
+     * 
+     * @param hosAdress
+     * @param port
+     * @param userViewIn 
+     */
+    public ClientConnection(String hosAdress, int port, UserView userViewIn) {
+        myUserView = userViewIn;
+    }
+       
+    /**
+     * Used to send message to server which client is connected to.
+     * @param messageOut 
+     */
+    public void sendMessage(String messageOut) {
+        
+    }
     
+    /**
+     * 
+     */
+    public void run(){
+        
+    }
+
 }
--- CoolChat/src/coolChat/ServerConnection.java
+++ CoolChat/src/coolChat/ServerConnection.java
@@ -1,14 +1,51 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ClientConnection
+ * 
+ * V 1.0
+ *
+ * 2018-01-17
+ * 
+ * Copyright notice
  */
 package coolChat;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 /**
- *
- * @author klasern
+ * Establish connection with a client which connects to the server of 
+ * this client.
  */
 public class ServerConnection extends Thread {
+    
+    private List<ChatListener> inListen;
+    private List<PrintWriter> outPut;
+    private Socket clientSocket;
+    private UserView myUserView;
+    
+    public boolean isGroupChat = false;
+    
+    /**
+     * Adds pointer to UserView and clientsocket.
+     * @param clientSocketIn
+     * @param userViewIn 
+     */
+    public ServerConnection(Socket clientSocketIn, UserView userViewIn){
+        myUserView = userViewIn;
+        clientSocket = clientSocketIn;        
+    }
+     
+    /**
+     * Sends message to all clients connected to this server.
+     * @param messageOut 
+     */
+    public void sendMessage(String messageOut) {
+        
+    }
+    
+    public void run(){
+        
+    }
     
 }
