1217fdce4ca36d82d2caa7afa2f97df2d2df985d
klasern <klasern@karmosin-12.csc.kth.se>
2018-02-14 09:13:38 +0100

    Test

--- CoolChat/src/coolChat/UserView.java
+++ CoolChat/src/coolChat/UserView.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * UserView
+ * 
+ * V 1.0
+ *
+ * 2018-01-17
+ * 
+ * Copyright notice
  */
 package coolChat;
 
@@ -9,25 +13,75 @@ import java.awt.event.*;
 import java.net.*;
 import java.util.*;
 import javax.swing.*;
+
 /**
- *
- * @author klasern
+ * Main GUI and keeps track of all connections.
  */
 public class UserView extends JFrame implements ActionListener {
-    
+
+    /*GUI*/
     private JTabbedPane myTabbedPane;
+    private JButton connectButton;
+    private JComboBox kickComboBox;
+    private JComboBox disconnectComboBox;
+
+    /*Chat objects*/
     private List<Chat> chats;
+
+    /* Connections */
     private List<Socket> clients;
     private List<ClientConnection> connectedTo;
     private List<ServerConnection> connections;
-    private JButton connectButton;
     private ServerThread serverThread;
+    public static Boolean groupChatActive;
+
+    /**
+     * Creates the UserView.
+     */
+    public UserView() {
+
+    }
+
+    /**
+     * Adds the chat to the JTabbedPane
+     *
+     * @param chatIn
+     */
+    public void addChat(Chat chatIn) {
+
+    }
+
+    /**
+     * Connects to another IP for chat.
+     */
+    private void connect() {
+
+    }
+
+    /**
+     * Disconnects from a chat.
+     */
+    private void disconnectChat() {
+
+    }
     
+    /**
+     * Kicks a client from server.
+     */
+    private void kickClient() {
+        
+    }
     
+    /**
+     * Removes a chat.
+     */
+    private void removeChat() {
+        
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
