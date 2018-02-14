e5167282dee963da6cdd4312335942ca637b1aa9
klasern <klasern@karmosin-12.csc.kth.se>
2018-02-14 09:42:52 +0100

    Revert "Test"
    This reverts commit 257b0d32d2df3f212dff33a4e3e7f4b809b446a3.

--- CoolChat/src/coolChat/TemporaryConnection.java
+++ CoolChat/src/coolChat/TemporaryConnection.java
@@ -1,76 +1,14 @@
 /*
- * UserView
- * 
- * V 1.0
- *
- * 2018-01-17
- * 
- * Copyright notice
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package coolChat;
 
-import java.io.*;
-import java.net.*;
-
 /**
- * Creates a temporary connection which asks if we want client to establish a
- * connection or not.
+ *
+ * @author klasern
  */
 public class TemporaryConnection extends Thread {
-
-    private BufferedReader in;
-    private PrintWriter out;
-    private UserView myUserView;
-
-    /**
-     * Gets pointer to UserView and creates a new thread.
-     *
-     * @param clientSocketIn
-     * @param myUserView
-     */
-    public TemporaryConnection(Socket clientSocketIn, UserView myUserView) {
-
-    }
-
-    /**
-     * Adds client to groupchat.
-     *
-     * @param clientSocketIn
-     */
-    private void addToGroup(Socket clientSocketIn) {
-
-    }
     
-    /**
-     * Create new single chat.
-     * @param clientSocketIn 
-     */
-    private void createNewChat(Socket clientSocketIn) {
-
-    }
-    
-    /**
-     * Denies the connection which tries to connect.
-     */
-    private void denyConnection(){
-        
-    }
-    
-    /**
-     * Checks if connecting client is a noob client.
-     * @return 
-     */
-    private boolean isNoobClient(){
-        return true;
-    }     
-    
-
-    /**
-     * Used to ask if client should be added to group chat, single chat or
-     * delete connection.
-     */
-    public void run() {
-
-    }
-
 }
