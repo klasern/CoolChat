da1a6654c9b73c974306c8203f0c0f2d7e9a1215
klasern <klasern@karmosin-12.csc.kth.se>
2018-02-14 10:42:21 +0100

    Starts to build up GUI in UserView, and begins writing the code for the ServerThread and TemporaryConnection

--- CoolChat/src/coolChat/ServerThread.java
+++ CoolChat/src/coolChat/ServerThread.java
@@ -9,41 +9,71 @@
  */
 package coolChat;
 
+import java.io.*;
 import java.net.*;
+import javax.swing.*;
 
 /**
- * Creates a thread which constantly listens to incomming connections on a 
+ * Creates a thread which constantly listens to incomming connections on a
  * ServerSocket.
- */ 
-public class ServerThread extends Thread{
-    
+ */
+public class ServerThread extends Thread {
+
     /*The port we listens to*/
     private ServerSocket serverSocket;
     private int port;
     private UserView myUserView;
-    
+
     /**
-     * Constructor used to get pointer to UserView.
-     * @param userViewIn 
+     * Constructor used to get pointer to UserView, and start the thread which
+     * listens to the serversocket.
+     *
+     * @param userViewIn
      */
-    public ServerThread(UserView userViewIn){
-        
+    public ServerThread(UserView userViewIn) {
+        myUserView = userViewIn;
     }
-    
+
     /**
      * Used to start TemporaryConnection.
      */
-    private void startTemporaryConnection(){
-        
+    private void startTemporaryConnection() {
+
     }
-       
-    
+
     /**
      * Ask which port to listen to and listen to that port.
      */
-    public void run(){
-        
+    public void run() {
+
+        //Gets port to listen to.
+        port = Integer.parseInt(
+                JOptionPane.showInputDialog(myUserView, 
+                        "What port to listen to?"));
+
+        //Starts server socket
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println("Could not listen on port: " + port);
+            System.exit(-1);
+        }
+
+        while (true) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("Accept failed: " + port);
+                System.exit(-1);
+            }
+            Thread temporaryThread = new TemporaryConnection(clientSocket, 
+                    myUserView);            
+            temporaryThread.start();            
+            
+            
+        }
+
     }
 
-    
 }
